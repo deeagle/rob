ad5333e8e3a04c1dacb6372169ce7eaa29332230
Martin Kock <code@deeagle.de>
2021-07-15 10:24:56 +0200

    add: read files and select by prefix

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 # This is a sample Python script.
 import configparser as configparser
+import os
+import time
 
+CONFIG_FILE_NAME = 'config.ini'
 CONF_COMMON_KEY = 'Common'
 CONF_COMMON_KEEP_FILES_KEY = 'keep_files'
 CONF_COMMON_KEEP_FILES = 10
 CONF_COMMON_KEEP_PATH_KEY = 'keep_path'
 CONF_COMMON_KEEP_PATH = '.'
+CONF_BACKUP_FILE_PREFIX = 'NOTHING-SET'
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -14,8 +19,13 @@ def print_hi(name):
 
 def load_config():
     global CONF_COMMON_KEEP_FILES, CONF_COMMON_KEEP_PATH
+    if not os.path.exists(CONFIG_FILE_NAME):
+        print("[ERR!] Config file <{}> does not exists.".format(CONFIG_FILE_NAME))
+        return
+
+    print("[INFO] Config file found. Loading values.")
     config = configparser.ConfigParser()
-    config.read('config.ini')
+    config.read(CONFIG_FILE_NAME)
     print(config.sections())
     if CONF_COMMON_KEEP_FILES_KEY in config[CONF_COMMON_KEY]:
         CONF_COMMON_KEEP_FILES = config[CONF_COMMON_KEY][CONF_COMMON_KEEP_FILES_KEY]
@@ -23,8 +33,23 @@ def load_config():
         CONF_COMMON_KEEP_PATH = config[CONF_COMMON_KEY][CONF_COMMON_KEEP_PATH_KEY]
 
 
+def print_directory(path):
+    if not os.path.exists(path):
+        print("[ERR!] Path <{}> does not exists.".format(path))
+        return
+
+    print("[INFO] Path <{}> exists.".format(path))
+    print("[INFO] Search for files with prefix <{}>".format(CONF_BACKUP_FILE_PREFIX))
+    l_files = os.listdir(path)
+    for file in l_files:
+        if file.startswith(CONF_BACKUP_FILE_PREFIX):
+            print("Prefix yes: {}".format(file))
+        else:
+            print("Prefix  no: {}".format(file))
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     load_config()
-    print(CONF_COMMON_KEEP_FILES)
+    print_directory(CONF_COMMON_KEEP_PATH)
