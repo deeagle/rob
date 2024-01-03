0337d80f70aaef645079080812dec87b2199ebe1
Martin deeagle Kock <mko@leibniz-psychology.org>
2024-01-03 12:24:24 +0100

    ci(local): use flake8 within docker

--- run-ci.sh
+++ run-ci.sh
@@ -16,8 +16,8 @@ docker run --rm -v "$(pwd):/app" koalaman/shellcheck-alpine:stable find /app -ty
 echo "Lint code (flake8) ..."
 echo "> we want O's"
 # stop the build if there are Python syntax errors or undefined names
-flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
+docker run --rm -v "$(pwd):/app" pipelinecomponents/flake8 flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
 # exit-zero treats all errors as warnings.
-flake8 . --count --max-complexity=10 --max-line-length=120 --statistics
+docker run --rm -v "$(pwd):/app" pipelinecomponents/flake8 flake8 . --count --max-complexity=10 --max-line-length=120 --statistics
 
 echo "CI successfully finished."
